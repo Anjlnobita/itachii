4dca33e201955b20f78b53440680478d8eab99b3
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-20 19:57:45 +0530

    Update bot.py

--- JioSavaan/core/bot.py
+++ JioSavaan/core/bot.py
@@ -15,8 +15,7 @@ class Anony(Client):
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
             in_memory=True,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
+         max_concurrent_transmissions=7,
         )
 
     async def start(self):
@@ -49,6 +48,30 @@ class Anony(Client):
             )
             exit()
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+
+    async def stop(self):
+        await super().stop()
+
+
+
+
+class nobita(Client):
+    def __init__(self):
+        super().__init__(
+            name="meow",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
 
     async def stop(self):
         await super().stop()
