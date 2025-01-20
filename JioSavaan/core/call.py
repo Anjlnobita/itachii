1c42bebf68ed3cfddd4d0e79129030293132da06
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-21 00:33:23 +0530

    Update call.py

--- JioSavaan/core/call.py
+++ JioSavaan/core/call.py
@@ -580,21 +580,21 @@ class Call(PyTgCalls):
         @self.three.on_closed_voice_chat()
         @self.four.on_closed_voice_chat()
         @self.five.on_closed_voice_chat()
-        @self.one.on_left_voice_chat()
-        @self.two.on_left_voice_chat()
-        @self.three.on_left_voice_chat()
-        @self.four.on_left_voice_chat()
-        @self.five.on_left_voics_chat()
+        @self.one.on_left()
+        @self.two.on_left()
+        @self.three.on_left()
+        @self.four.on_left()
+        @self.five.on_left()
         async def stream_services_handler(_, chat_id: int):
             await self.stop_stream(chat_id)
 
-        @self.one.on_audio_stream_end()
-        @self.two.on_audio_stream_end()
-        @self.three.on_audio_stream_end()
-        @self.four.on_audio_stream_end()
-        @self.five.on_audio_stream_end()
+        @self.one.on_stream_end()
+        @self.two.on_stream_end()
+        @self.three.on_stream_end()
+        @self.four.on_stream_end()
+        @self.five.on_stream_end()
         async def stream_end_handler1(client, update: Update):
-            if not isinstance(update, AudioStreamEnd):
+            if not isinstance(update, StreamAudioEnded):
                 return
             await self.change_stream(client, update.chat_id)
 
