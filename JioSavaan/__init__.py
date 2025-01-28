8ee7dd1bb3d9045ac34c13b0525160a4b54d16c1
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-28 05:37:16 +0530

    Update __init__.py

--- JioSavaan/__init__.py
+++ JioSavaan/__init__.py
@@ -1,7 +1,7 @@
 from JioSavaan.core.bot import Anony
 from JioSavaan.core.dir import dirr
 from JioSavaan.core.git import git
-from JioSavaan.core.userbot import Userbot
+#from JioSavaan.core.userbot import Userbot
 from JioSavaan.misc import dbb, heroku
 
 from .logging import LOGGER
@@ -12,7 +12,7 @@ dbb()
 heroku()
 
 app = Anony()
-userbot = Userbot()
+#userbot = Userbot()
 
 from .platforms import *
 
